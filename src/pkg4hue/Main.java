d999347ee08006aad728f5119fcc432554e5656a
fgaisboeck <GaisboeckF190055@sus.htl-grieskirchen.at>
2021-11-14 18:59:51 +0100

    sunday

--- src/pkg4hue/Main.java
+++ src/pkg4hue/Main.java
@@ -1,20 +1,14 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package pkg4hue;
 
-/**
- *
- * @author gaisb
- */
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
+        List<Integer> zahlen = new ArrayList<>();
+        Scanner sc = new Scanner("numbers.csv");
 
+    }
 }
